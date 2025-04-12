1a079251d834debaaa79a8b4a33cd2f72e854652
boldak <boldak.andrey@gmail.com>
2025-04-12 21:13:03 +0300

    add cli

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ const proxyServer = require("./src/proxy-server")
 
 const ECHO_PORT = 3001
 
-proxyServer.listen(proxy.PORT, console.log(`PROXY Server starts on ${proxy.PORT} port`));
-echoServer.listen(ECHO_PORT, console.log(`ECHO Server starts on ${ECHO_PORT} port`));
+let needStart = process.argv[2]
+if(needStart == "echo"){
+	echoServer.listen(ECHO_PORT, console.log(`ECHO Server starts on ${ECHO_PORT} port`));
+}
+if(needStart == "proxy"){
+	proxyServer.listen(proxy.PORT, console.log(`PROXY Server starts on ${proxy.PORT} port`));
+}
+if(!needStart){
+	proxyServer.listen(proxy.PORT, console.log(`PROXY Server starts on ${proxy.PORT} port`));
+	echoServer.listen(ECHO_PORT, console.log(`ECHO Server starts on ${ECHO_PORT} port`));
+}	
